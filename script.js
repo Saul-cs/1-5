88283a53d8412158d57c06b34b8c6fdb95ab9ac3
Saul-cs <124259775+Saul-cs@users.noreply.github.com>
2024-11-25 21:09:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let attemptsLeft = 3; // Total de tentativas permitidas
+let warningPlayed = false; // Controle para tocar o som apenas uma vez
+
+// Função de verificação
+function verifyAccess() {
+    const accessCode = document.getElementById("accessCode").value;
+    const errorMessage = document.getElementById("errorMessage");
+
+    // Códigos de acesso válidos
+    const codes = {
+        junior: "11-642",
+        fhelipe: "54-510"
+    };
+
+    if (accessCode === codes.junior) {
+        window.location.href = "junior.html";
+    } else if (accessCode === codes.fhelipe) {
+        window.location.href = "fhelipe.html";
+    } else {
+        attemptsLeft--;
+
+        if (attemptsLeft === 1 && !warningPlayed) {
+            playWarningSound();
+            warningPlayed = true;
+        }
+
+        if (attemptsLeft > 0) {
+            errorMessage.textContent = `Código de acesso inválido. Você tem ${attemptsLeft} tentativa(s).`;
+        } else {
+            errorMessage.textContent = "tente novamente.";
+            document.getElementById("accessCode").disabled = true;
+        }
+    }
+}
+
+// Função para tocar o som de aviso
+function playWarningSound() {
+    const audio = new Audio("audios/frase.mp3");
+    audio.play();
+}
+
+// Adicionando o evento ao botão
+document.getElementById("loginButton").addEventListener("click", verifyAccess);
